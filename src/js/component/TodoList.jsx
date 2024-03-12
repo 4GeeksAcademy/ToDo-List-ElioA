b448762e36f87fc9661aa67b280b3e1d8f86af8b
ElioAloise <155248934+ElioAloise@users.noreply.github.com>
2024-03-12 19:08:13 +0000

    final commit

--- src/js/component/TodoList.jsx
+++ src/js/component/TodoList.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const initialList = ["going to the doctor", "getting pet food"];
+const initialList = ["Going to the doctor", "Getting pet food"];
 
 const TodoList=()=>{
 
@@ -8,7 +8,7 @@ const [list, setList ]=useState(initialList);
 const [name, setName] = useState('');
 
 const addTodo=()=>{
-    setList(currentList=>[...currentList, name])
+    if(name!=="")setList(currentList=>[...currentList, name[0].toUpperCase()+name.slice(1)])
     setName("")
 }
 
@@ -17,19 +17,30 @@ const deleteTodo=(taskDone)=>{
     setList(newlist);
 }
 
+const remainingTasks=()=>{
+    if(list.length==0){return <p>Add a task to the List</p>}
+    else {return <p>{list.length} Tasks remain on the list</p>}
+    
+}
+
 return (
-        <div className="card" style={{width:"600px"}} id="list">
-            <h1 className="d-flex justify-content-center m-3">My To-do list</h1>
-            <form className="d-flex justify-content-center m-3">
-                <div className="mb-3">    
-                <input  type="text" className="form-control"  placeholder="Add todo" aria-label="StartingValue" value={name} onChange={(e)=>setName(e.target.value)}/>
-                <button type="button" style={{marginTop:"10px"}} className="btn btn-success" onClick={()=>addTodo()}>add ToDo</button>
-                </div>
+        <div className="card"  id="list">
+            <div class="card-header text-center">
+                <h1 className="d-flex justify-content-center m-3">My Todo list</h1>
+            </div>
+            <form className="todo-form d-flex justify-content-center">
+                    
+                <input  type="text" className="form-control todo-input"  placeholder="Add todo" aria-label="StartingValue" value={name} onChange={(e)=>setName(e.target.value)}/>
+                <button type="button" className="btn btn-success todo-button" onClick={()=>addTodo()}>Add Task</button>
+                
             </form>
             <ul>
-                 {list.map((item, index) => (<li className="myLi" key={index} style={{marginTop:"20px"}}>{item}
+                 {list.map((item, index) => (<li className="todo-row" key={index} style={{marginTop:"20px"}}>{item}
                  <span className="rounded" onClick={()=>deleteTodo(item)}><i className="fas fa-trash"></i></span></li>))}
             </ul>
+            <div class="card-footer" style={{marginTop:"10px", color:"white"}} >
+                 <p>{remainingTasks()}</p>
+            </div>
         </div>    
         );
 }
